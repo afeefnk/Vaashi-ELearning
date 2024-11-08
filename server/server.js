5b915977b7e4ae6d6262e7e2aaad03ea210af876
afeefnk <connectafeefnk@gmail.com>
2024-11-08 10:19:49 +0530

    Initial Push

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import courseRouter from "./routes/course-routes.js";
+import router from "./routes/user-routes.js";
+import cors from "cors";
+
+const app = express();
+
+const db =
+  "mongodb+srv://connectafeefnk:60qp3J91rg7KtorD@vaashi-elearning.wlagu.mongodb.net/?retryWrites=true&w=majority&appName=Vaashi-ELearning";
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user", router);
+app.use("/api/course", courseRouter);
+mongoose
+  .connect(db)
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected to database and listening to localhost:5000")
+  )
+  .catch((err) => console.log(err));
